aa08f3d95c1b2b1c3e4a6dd366ba372c9e25cfb1
NDinhChien <godloveyou251101@gmail.com>
2023-03-28 12:38:46 +0700

    fix function getOption

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 typedef unsigned int uint;
 
 // một số hàm hỗ trợ
+// copy n bytes from src starting at k
 BYTE* copy(BYTE* src, int k, int n) {
   BYTE* dest = new BYTE[n];
   for(int i=0; i<n; i++) {
@@ -20,7 +21,7 @@ BYTE* copy(BYTE* src, int k, int n) {
 } 
 uint e256[4] = {1, 256, 256*256, 256*256*256};
 
-// litte edian, normal indexing
+// convert a litte edian number to a interger 
 uint convert(BYTE number[], int n) {  // 4 BYTE trở xuống
   uint t = 0;
   for (int i=0; i<n; i++) {
@@ -28,36 +29,35 @@ uint convert(BYTE number[], int n) {  // 4 BYTE trở xuống
   }
   return t;
 }
-
+// convert a 16 bit string to a date
 string toDate(BYTE date[]) { 
-	bitset<7> y;  // 6...0
-	bitset<4> m;  // 3...0
-	bitset<5> d;  // 4...0
-	bitset<8> r(date[0]); // date: 0...7      8...15
-	bitset<8> l(date[1]); //    l: 7...0   r: 7...0 
-	
-	for (int i=6; i>=0; i--) y[i] = l[i+1]; // 7...1
+	bitset<7> y;   
+	bitset<4> m;  
+	bitset<5> d;  
+	bitset<8> r(date[0]); 
+	bitset<8> l(date[1]);  
+	for (int i=6; i>=0; i--) y[i] = l[i+1]; 
 	m[3] = l[0];
-	for (int i=2; i>=0; i--) m[i] = r[i+5]; // 7...5
-	for (int i=4; i>=0; i--) d[i] = r[i]; // 4...0
+	for (int i=2; i>=0; i--) m[i] = r[i+5]; 
+	for (int i=4; i>=0; i--) d[i] = r[i]; 
 	string str(to_string(y.to_ulong()+1980) + "-"+ to_string(m.to_ulong())+ "-"+ to_string(d.to_ulong()));
 	return str;
-
 }
+// convert a 16 bit string to a time
 string toHour(BYTE hour[]) {
-    bitset<5> h;  // 4...0
-	bitset<6> m;  // 5...0
-	bitset<5> s;  // 4...0
-	bitset<8> r(hour[0]); // date: 0...7      8...15
-	bitset<8> l(hour[1]); //    l: 7...0   r: 7...0 
-	
-	for (int i=4; i>=0; i--) h[i] = l[i+3]; // 7...3
+    bitset<5> h;  
+	bitset<6> m;  
+	bitset<5> s;   
+	bitset<8> r(hour[0]);  
+	bitset<8> l(hour[1]);  
+	for (int i=4; i>=0; i--) h[i] = l[i+3]; 
 	m[5] = l[2]; m[4]= l[1]; m[3]= l[0];
-	for (int i=2; i>=0; i--) m[i] = r[i+5]; // 7...5
-	for (int i=4; i>=0; i--) s[i] = r[i]; // 4...0
+	for (int i=2; i>=0; i--) m[i] = r[i+5]; 
+	for (int i=4; i>=0; i--) s[i] = r[i]; 
 	string str(to_string(h.to_ulong()) + ":"+ to_string(m.to_ulong())+ ":"+ to_string(s.to_ulong()));
 	return str;
 } 
+// print str 
 void pChar(BYTE* str, int n, int ncol) {
 	int d = n/ncol;
 	int r = n%ncol;
@@ -72,6 +72,7 @@ void pChar(BYTE* str, int n, int ncol) {
 	}
 	if (r>0) cout << endl;	
 }
+// print str in hexa form
 void pHexa(BYTE* str, int n, int ncol) {
 	int t;
 	int d = n/ncol;
@@ -91,28 +92,27 @@ void pHexa(BYTE* str, int n, int ncol) {
 	}
 	if (r>0) cout << endl;	
 }
+// check if a char is a letter
 bool isLetterChar(char chr) {
 	if (('a'<=chr && chr <='z') || ('A'<=chr && chr <='Z')) return 1;
 	return 0;
 }
+// check if a char is a number letter
 bool isNumberChar(char chr) {
 	if ('0'<=chr && chr <='9') return 1;
 	return 0;
 }
-int getOption(int start, int end, int step, bool isletterAccepted) { // isletterAccepted 1, them mot so chu cai
+// get option, if isletterAccepted = 1, accept some letters: R/r, B/b
+int getOption(int start, int end, int step, bool isletterAccepted) { 
 	string rs;
 	int tmp, l;
 	bool isNumber;
 	int plus=isletterAccepted*2;  
-	// isletterAccepted=0, plus=0 // normal 
-	// isletterAccepted=1, plus=2; 
-	// b/B, r/R 0, 1, 2, 3
-	// 0,    1, 2  3, 4, 5
 	while(1) {  
 		cout <<">> ";
 		cin >> rs;
 		l = rs.length();
-		if (isletterAccepted==1 && l==1 && isLetterChar(rs[0])) { // nhap 1 chu cai
+		if (isletterAccepted==1 && l==1 && isLetterChar(rs[0])) {
 			if (rs[0]=='r' || rs[0]=='R') return 1;
 			if (rs[0]=='b' || rs[0]=='B') return 0;
 			continue;
@@ -126,7 +126,7 @@ int getOption(int start, int end, int step, bool isletterAccepted) { // isletter
 			}
 		}
 		if (isNumber) {
-			tmp = stoi(rs); // 0-9
+			tmp = stoi(rs);
 			if (tmp>=start && tmp<=end && (tmp-start)%step==0) return tmp+plus;
 		} 
 	}
@@ -139,46 +139,49 @@ protected:
   string updatedHour; 
   string updatedDate;  
   int size;  
+  Component* parent;
 public:
   Component() {};
   ~Component() {};
-  Component(string name, int fcluster) {  // for root folder
+  Component(string name, int fcluster, Component* parent) {  // for root folder
   	this->name = name;
     this->fcluster = fcluster;
+    this->parent = parent;
   }
-  Component(string name, int fcluster, string updatedHour, string updatedDate, int size) { // for file or sub folder
+  Component(string name, int fcluster, string updatedHour, string updatedDate, int size, Component* parent) { // for file or sub folder
     this->name = name;
     this->fcluster = fcluster;
     this->updatedHour = updatedHour;  
   	this->updatedDate = updatedDate; 
   	this->size = size;
+  	this->parent = parent;
   }
 
-  // virtual so it can allow overriding
   virtual int isFile() {return 0;};
   virtual int isFolder() {return 0;};
   virtual string getName() {return name;};
   virtual int getSize() {return size;};
-  virtual Component* getParent() {return NULL;}
-  virtual void load() {};    // updated content or list
-  virtual void display(){};  // properties + content or list
+  virtual Component* getParent() {return parent;}
+  virtual void load() {};    // update content or list
+  virtual void display(){};  // print properties + content (or list with folder)
   
   virtual void properties() {  
   	cout << name << endl;
   	cout << "First cluster: "<< fcluster<<endl;
   	cout << "Size         : "<< getSize() <<endl;
-  	if (name=="root") return;
+  	if (name=="root" && parent==NULL) return; // root folder don't have these following informations
 	cout << "Updated hour : " << updatedHour<<endl;
   	cout << "Updated date : " << updatedDate<<endl;
   }
   
-  // can be able to call methods of volume v
+  // methods of Volume v
   virtual int cToS(int k){return 0;};
   virtual BYTE* read(int vt, int k){return NULL;};
   virtual int* lookUpFAT(int fcluster, int &n){return NULL;};
   virtual Component** readFolderTree(int fcluster, Component* parent){return NULL;};
   virtual void readFileContent(int fcluster, int size){};
   virtual void status() {};
+  
   // for testing
   virtual void test() {};    
 }; 
@@ -191,20 +194,20 @@ private:
 public:
   File():Component() {};
   ~File() {};
-  File(string name, int fcluster, string updatedHour, string updatedDate, int size, bool isTextFile) 
-  	: Component(name, fcluster, updatedHour, updatedDate, size) {
+  File(string name, int fcluster, string updatedHour, string updatedDate, int size, Component* parent, bool isTextFile) 
+  	: Component(name, fcluster, updatedHour, updatedDate, size, parent) {
   		this->isTextFile = isTextFile;
   };
   void display() {  
   	system("cls");
-  	properties();                          // of this file, only update when refresh its parent (folder chứa nó)
-  	cout << "\n-----Content-----\n";       // content
+  	properties(); 
+  	cout << "\n-----Content-----\n";       
   	
 	if (isTextFile) {
 		if (size==0) {
 			cout << "<empty>\n";
 		}
-		else v->readFileContent(fcluster, size); // reload content 
+		else v->readFileContent(fcluster, size); 
 	}
 	else {
 		cout << "Xin loi, hien tai he thong chi co the doc noi dung file TXT !";
@@ -218,7 +221,6 @@ public:
     cout << "File name    : ";
     Component::properties();
   }
-  //void load() {};
   int isFile() {return 1;};
 };
 
@@ -226,22 +228,18 @@ public:
 class Folder :public Component{
 private:
   Component** list=NULL;  
-  Component* parent;    // name=="root" &* parent==NULL // 
+
 public:
   Folder():Component() {};
   Folder(string name, int fcluster,  Component* parent) 
-    :Component(name, fcluster) { // for root folder, fcluster = 2
-    this->parent = parent;       // NULL;
-  }
-  
+    :Component(name, fcluster, parent) {};                                 // for root folder, fcluster = 2, parent==NULL;
+      
   Folder(string name, int fcluster, string updatedHour, string updatedDate, int size, Component* parent) 
-    :Component(name, fcluster, updatedHour, updatedDate, size) {   // for sub folder
-  	this->parent = parent;
-  }
+    :Component(name, fcluster, updatedHour, updatedDate, size, parent) {}; // for sub folder
   int getSize() {
     if (list==NULL) return 0; 
     int total;
-  	for (int i=2; i<52; i++) { //
+  	for (int i=2; i<52; i++) { 
   		if (list[i]!=NULL) {
   			total += list[i]->getSize();
 		}
@@ -249,19 +247,17 @@ public:
 	}
 	return total;
   }
-  Component* getParent() {
-  	return parent;
-  }
-  void display() {                   // properties and list
-  	load();                          // reload list // all files and subfolder properties are updated// the first thing
+
+  void display() {                    
+  	load();        // reload list // all files and subfolder properties are updated 
     int myoption;
     while(1) { 
       system("cls");
       properties();
-	  cout << "\n-----Content-----\n";                       
+	  cout << "\n-----Content-----\n";   
+	  cout << name << endl;                     
       cout << "  B. Back\n";
       cout << "  R. Refresh\n\n";
-	  cout << name<< endl; 
       int i=2;
 	  for (; i<52; i++) {
 	  	if (list[i]!=NULL) {
@@ -269,23 +265,21 @@ public:
 		}
 		else break;
 	  }
-	  if (i==2) cout << "<empty>\n";
-	  
-	  myoption = getOption(0, i-1, 1, true); // r/R->0, b/B->1, 0->i-1 (+2) // tuong ung
+	  if (i==2) {
+	  	cout << "<empty>\n\n";	
+	  }
+	  myoption = getOption(0, i-3, 1, true); 
       if (myoption==0) return;
-	  if (myoption==1) load(); // list duoc cap nhat
+	  if (myoption==1) load();  
 	  else list[myoption]->display(); 
 	}
   }
   
-  // SDET, bo 3 entry cua '.' va '..' (wrong entry) 
-  // root, parent, this, chi co 1 version, duoc cap nhat chu, khi refresh parent chua no, nhung root khong duoc cap nhat (khong can thiet, fcluster=2 always)	
   void load() {  
   	freeMemory();  
   	list = v->readFolderTree(fcluster, this);   
   	list[0] = parent; 
  	list[1] = this; 
-	
   }
   void countChildren() {
   	int nfi=0,nfo=0;
@@ -308,18 +302,15 @@ public:
   int isFolder() {return 1;};
 
   void freeMemory() {
-	// not for the first time
 	if (list==NULL) return;
-	// don't delete parent (root in danger), don't delete this (xoa chinh doi tuong goi load())
-  	for (int i=2; i<52; i++) { 
+	for (int i=2; i<52; i++) { 
     	if (list[i]!=NULL) delete list[i];
     	else break;
 	}
-	delete[] list; // 
-	list = NULL;   // lan dau và sau khi free Memory (khong free tiep)
+	delete[] list; 
+	list = NULL;
   }
   ~Folder() {
-  	delete parent;
   	freeMemory();
   }
 };
@@ -330,8 +321,8 @@ private:
   	char volume_format[7]; 
     HANDLE device = NULL;
     int nS;                          // nS=512;
-    int sC, sB, nF, sF, sV;          //sC=4, sB=6646, nF=2, sF=773, sV=403456; 
-    int fSectorOfFAT, fSectorOfRDET; //fSectorOfFAT = 6646,  fSectorOfRDET = 8192;
+    int sC, sB, nF, sF, sV;          // sC=4, sB=6646, nF=2, sF=773, sV=403456; 
+    int fSectorOfFAT, fSectorOfRDET; // fSectorOfFAT = 6646,  fSectorOfRDET = 8192;
     Component* root = NULL;  
 public:
 	Volume():Component() {};
@@ -374,11 +365,9 @@ public:
 	    do {
 			cout << "Nhap volume letter (vd E): ";
 			cin >>  letter;
-			
 			my_volume[4] = letter;
 			my_volume[5] = ':';
 			my_volume[6] = '\0';
-			
 			//cout << my_volume<<endl;
 			device = CreateFile(my_volume,             // Drive to open  // volume_letter
 		                        GENERIC_READ,           // Access mode
@@ -392,7 +381,6 @@ public:
 	    		cout << "That bai! Volume "<< letter <<" khong ton tai hoac co dinh dang khac FAT32\n";
 			}
 			else {
-				
 				break;
 			}
 		} while (1);  
@@ -407,16 +395,13 @@ public:
 		sC = convert(copy(bootsector, 0xd, 1), 1);  // (số sector/cluster)
 		nF = convert(copy(bootsector, 0x10, 1), 1); // (số bảng FAT)
 		sF = convert(copy(bootsector, 0x24, 4), 4); // (số sectors/FAT)
-	
 		strcpy(volume_format, (char*)copy(bootsector, 0x52, 6));  //  chuỗi 'FAT32';
-		
 		fcluster = convert(copy(bootsector, 0x2C, 4), 4);
 		fSectorOfFAT = sB;                          // bootsector, FAT, FAT2, Data Area(RDET, ...)
 		fSectorOfRDET = cToS(fcluster); 
 	    delete[] bootsector; 
 	}
-	    
-
+	
 	void properties() {
 		system("cls");
 		if (device==NULL) {
@@ -439,11 +424,11 @@ public:
 	
 	~Volume(){CloseHandle(device);};
 	
-	// reader
+	// convert cluster index to sector index
 	int cToS(int k) {
     	return sB + nF*sF + (k-2)*sC;  
     }
-    // đọc k bytes từ vị trí vt
+    // read k bytes starting from vt
 	BYTE* read(int vt, int k) {      
   		DWORD bytesRead;
   		BYTE* buffer = new BYTE[k]; 
@@ -453,24 +438,23 @@ public:
   		}
   		return buffer;
 	}
-    // doc bang FAT chua fcluster
+    // read FAT table contains the fcluster(th) cluster
 	BYTE* readFAT(int fcluster, uint &d, uint &r) {	
 		d = fcluster/(nS/4);        
 		r = fcluster%(nS/4);        
 		return read((fSectorOfFAT+d)*nS, nS);  // FAT đánh số từ 0, 128 cluster, 256 KB 
 	}
-	// tra cuu bang fat, tra ve danh sach cac sectors
+	// look up fat table, return a list of sectors
 	int* lookUpFAT(int fcluster, int &n) {   
 	    vector<uint> clusters;
+	    uint t = fcluster; // the actual cluster
 	    uint d, r;         // tương ứng bảng fat thứ d, cluster thứ r   
-	    uint t = fcluster; // the real cluster thứ  
 		BYTE* fat = readFAT(fcluster, d, r);
-		while(d!=0 || r>=2) {      // d==0 && r<2 
+		while(d!=0 || r>=2) {      // d==0 && r<2 (không đọc 2 cluster đầu tiên)
 			clusters.push_back(t); // oke
 			//cout << t << endl; 
 			t = convert(copy(fat, r*4, 4), 4); // bảng fat thứ d, cluster thứ r  
 			if (t >= 0x0FFFFFF8) break;        // 0x0FFFFFFF, 0x0FFFFFF8 // end
-			
 			if (t/(nS/4)!=d) {                 // có cần đọc lại bảng fat (!= d hiện tại)
 				delete[] fat;
 				fat = readFAT(t, d, r);        // đọc lại bảng fat, d, r được cập nhật (ứng với đoạn fat mới)
@@ -478,11 +462,9 @@ public:
 			else {
 				r = t%(nS/4);
 			}
-			//cout << "hello" << endl;
 		};
-		n = clusters.size()*sC; // sC=4;
+		n = clusters.size()*sC; // sC=4 
 		int* s = new int [n]; 
-		
 		for (int i=0; i<clusters.size(); i++) {
 			t = cToS(clusters[i]);
 			for (int j=0; j<sC; j++) {
@@ -492,12 +474,12 @@ public:
 		delete[] fat;
 		return s;	
 	}
-    // doc cay thu muc, tra ve danh sach files va subfolders
+    // read folder tree, return a list of files and subfolders
     Component** readFolderTree(int fcluster, Component* parent) {  
     	int n; 
-  	 	int* s = v->lookUpFAT(fcluster, n);      // for RDET or SDET // deu doc tu dau bang (index khac)
-  	 	
-		Component** list = new Component* [52];  // tối da 50 thư mục con  //  con trỏ cũng nhẹ
+  	 	int* s = v->lookUpFAT(fcluster, n);      // for RDET or SDET 
+  	
+		Component** list = new Component* [52];  // tối đa 50 thư mục con 
 		for (int i=0; i<52; i++) list[i] = NULL; 
 		int index = 2;                       
 		
@@ -528,7 +510,7 @@ public:
 					//cout << "full name: "<< name<<endl;
 					int j = t-32;                // entry truoc do, cot 0
 					int jb = t-32 + 0xb;         // entry truoc do, cot b  j, jb trên một hàng
-					if (j>=0 && det[j]!=0xE5 && det[j]!=0 && jb>=0 && det[jb]==0xf) { // có it nhat mot entry phu
+					if (j>=0 && det[j]!=0xE5 && jb>=0 && det[jb]==0xf) { // có it nhat mot entry phu
 					    name = "";
 					    do {
 					    	for (int k=0; k<5; k++) name += det[j+1+2*k];   // 1
@@ -553,7 +535,7 @@ public:
 						if (ext == "TXT") {
 							isTextFile = true;
 						}
-						list[index] = new File(name, fclus, updatedHour, updatedDate, size, isTextFile);
+						list[index] = new File(name, fclus, updatedHour, updatedDate, size, parent, isTextFile);
 					}
 					index++;			
 				} 			
@@ -597,7 +579,7 @@ private:
     const char* list[4] = {"Exit", "Load volume", "Volume property", "Root folder"};;
 public: 
 	Menu() {};
-	void introduce() { // gioi thieu ung dung, thanh vien nhom
+	void introduce() {  
 		system("cls");
 		cout << "-----Introduce-----\n";
     	cout << "He Dieu Hanh Thay Long!\n";
@@ -645,33 +627,6 @@ int main() {
   menu->display();
   return 0;
 }
-
- 
-// files và subfolder (thông tin từ entry), read RDET (root folder, khong co thong tin tu entry)
-// sau này: load master boot record (lấy thông tin các FAT32 partion) 
-//volume size:   200MB * 1024(KB) * 1024(bytes) = 209,715,200 bytes
-//sector size:   512 bytes -> 409,600 sectors
-//clusters size: 2048 bytes (4 sectors) -> 102,400 clusters
- 
-
-/*
-
-Cho Volume kế thừa Component, File & Folder dùng qua component* v( but it's not really) // pass compiler 
-
-/*
-really important với classs
-không dễ để Parent* p = new Child;
-p lấy được các thuộc tính của
--> dùng virtual function Get...(), oke
-virtual = 0; không thể tạo đối tượng luôn, bắt buộc phải define hàm ở các con
-virtual, accessable các con, cho phép overriding
-no virtual, accessable các con, không cho phép overriding
--> dùng method để access child's property or manipulate on it :)))
-*/
-/*
-một số hàm không gọi trực tiếp thì khỏi virtual;
-*/
-
 
 
 
